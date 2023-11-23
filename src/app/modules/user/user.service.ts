2ec7f9582e047fdad5938dd84707ec703b5b87d0
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2023-11-23 22:54:21 +0600

    updated the get user method

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,15 +1,24 @@
 import { TUser } from './user.interface';
 import { UserModel } from './user.model';
 
+//create a new user method
 const createUserIntoDB = async (user: TUser) => {
   const result = await UserModel.create(user);
   return result;
 };
 
+//Get user method
 const getAllUserFromDB = async () => {
-  const result = await UserModel.find().select(
-    'username fullName.firstName fullName.lastName age email address.street address.city address.country',
-  );
+  const result = await UserModel.find().select({
+    username: 1,
+    'fullName.firstName': 1,
+    'fullName.lastName': 1,
+    age: 1,
+    email: 1,
+    'address.street': 1,
+    'address.city': 1,
+    'address.country': 1,
+  });
   return result;
 };
 
