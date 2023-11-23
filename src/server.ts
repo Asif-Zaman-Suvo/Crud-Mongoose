1a4f99cfa698e4cccc17c8d44504b9e92377039d
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2023-11-23 15:45:08 +0600

    config issue solve

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import config from './app/config';
 import mongoose from 'mongoose';
 
 async function main() {
-  await mongoose.connect(config.database_url as string);
   try {
+    await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
