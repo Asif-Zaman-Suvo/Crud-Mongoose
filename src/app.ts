cca84d69fee3af1a772556264c676836691597fb
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2023-11-25 22:03:26 +0600

    Update route route with HTML

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,48 @@ app.use(cors());
 app.use('/api', UserRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send(
-    'Welcome to Programming Hero level 2 course --- Assignment 2 , Student: Md Asifuzzaman Suvo',
-  );
+  const htmlContent = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Welcome to Programming Hero</title>
+      <style>
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: 'Arial', sans-serif;
+          background-color: #f4f4f4;
+        }
+
+        .welcome-container {
+          text-align: center;
+          padding: 50px;
+          margin: 50px auto;
+          background-color: #fff;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          border-radius: 5px;
+        }
+
+        h1 {
+          color: #333;
+        }
+
+        p {
+          color: #666;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="welcome-container">
+        <h1>Welcome to Programming Hero Level 2 Course - Assignment 2</h1>
+        <p>This is a simple web application built with Express, TypeScript, and CORS.</p>
+      </div>
+    </body>
+  </html>
+`;
+  res.send(htmlContent);
 });
 
 export default app;
